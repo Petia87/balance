3a9f47ba16602520035b3346e3b4113e10ac14e1
Petia Valchkova <peps@gmail.com>
2021-08-08 15:47:49 +0300

    nav

---
+++ src/views/Profile.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <h1 class="display-1">ddddddd</h1>
+        
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
